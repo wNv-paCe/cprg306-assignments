7d43b9e9116115d9859140cce5bbf370ea1f3c92
Charlie Kun <157261888+wNv-paCe@users.noreply.github.com>
2024-09-13 16:53:06 -0600

    Finish the week-2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Name: Kun Zheng</h1>
+      <p1>
+        <Link href="https://github.com/wNv-paCe/cprg306-assignments">
+          GitHub Link
+        </Link>
+      </p1>
+    </main>
+  );
+}
