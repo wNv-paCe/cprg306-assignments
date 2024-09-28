1148eb3f8b93ca9d8f478dc1abc929c3af3be600
Charlie Kun <157261888+wNv-paCe@users.noreply.github.com>
2024-09-27 20:09:58 -0600

    update style and color

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,8 +2,8 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <main>
-      <h1>Shopping List</h1>
+    <main className="bg-gray-800 flex min-h-screen flex-col items-center p-14">
+      <h1 className="text-white text-3xl font-bold mb-5">Shopping List</h1>
       <p1>
         <StudentInfo />
       </p1>
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   return (
-    <main>
-      <h1>Name: Kun Zheng</h1>
-      <p1>
+    <main className="w-48 h-16 p-2 flex flex-col justify-center">
+      <h1 className="text-white text-lg font-bold">Name: Charlie</h1>
+      <p1 className="text-rose-300 hover:text-orange-500 hover:underline">
         <Link href="https://github.com/wNv-paCe/cprg306-assignments">
           GitHub Link
         </Link>
